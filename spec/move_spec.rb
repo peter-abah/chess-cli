1cf11eef5589717f24d9e38ef995bf662240f3fb
Peter Abah <peterabah.ob@gmail.com>
2021-05-31 19:46:44 +0100

    add tests for Move class

---
+++ spec/move_spec.rb
@@ -0,0 +1,46 @@
+# frozen_string_literal: true
+
+describe Move do
+  let(:position) { [1, 9] }
+  let(:destination) { [3, 7] }
+
+  subject(:move) { described_class.new(position, destination) }
+
+  describe '#initialize' do
+    context 'when called with two arguments' do
+      it 'creates an object with @removed which is an empty array' do
+        result = move.instance_variable_get(:@removed)
+        expect(result).to eq([])
+      end
+
+      it 'creates an object with @moved which is a hash' do
+        result = move.instance_variable_get(:@moved)
+        expect(result).to be_a Hash
+      end
+
+      it '@moved atrribute has a key which is the first argument and the coreesponding value is the second argument' do
+        moved = move.instance_variable_get(:@moved)
+        result = moved[position]
+        expect(result).to eq(destination)
+      end
+    end
+  end
+
+  describe '#removed' do
+    context 'when called' do
+      it 'returns @removed atrribute' do
+        removed = move.instance_variable_get(:@removed)
+        expect(move.removed).to eq(removed)
+      end
+    end
+  end
+
+  describe '#moved' do
+    context 'when called' do
+      it 'returns @moved atrribute' do
+        moved = move.instance_variable_get(:@moved)
+        expect(move.moved).to eq(moved)
+      end
+    end
+  end
+end
