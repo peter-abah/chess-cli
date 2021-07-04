6b56d0a20acd92ddf6eb5c4ff9941afded635bb8
Peter Abah <peterabah.ob@gmail.com>
2021-07-04 23:38:21 +0100

    make Bishop class to inherit Piece class

--- lib/pieces/bishop.rb
+++ lib/pieces/bishop.rb
@@ -2,15 +2,16 @@
 
 require_relative '../move'
 require_relative '../move_generator'
+require_relative 'piece'
 
 # A class to represent a bishop in a chess game
-class Bishop
+class Bishop < Piece
   include MoveGenerator
 
-  attr_reader :color, :directions
+  attr_reader :directions
 
   def initialize(color)
-    @color = color
+    super
     @directions = [[1, 1], [-1, 1], [-1, -1], [1, -1]]
   end
 
