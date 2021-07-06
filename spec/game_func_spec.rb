487ab1c04fbc415bf3eb1bf4da04f7d8a5d430aa
Peter Abah <peterabah.ob@gmail.com>
2021-07-06 11:08:56 +0100

    add tests for #legal_move? in game_func_spec

--- spec/game_func_spec.rb
+++ spec/game_func_spec.rb
@@ -2,10 +2,38 @@
 
 require_relative '../lib/game_func'
 require_relative '../lib/board'
+require_relative '../lib/move'
 
 describe GameFunc do
   let(:dummy_class) { Class.new { extend GameFunc } }
   let(:board_array) { Board.new.board_array }
+
+  describe '#legal_move?' do
+    context 'when given a legal move' do
+      it 'returns true' do
+        move = Move.new([6, 3], [5, 3])
+        player = double(color: 'white')
+        board = Board.new
+
+        result = dummy_class.legal_move?(move, player, board)
+        expect(result).to be true
+      end
+    end
+
+    context 'when given an illegal move' do
+      it 'returns false' do
+        board_array[2][4] = board_array[0][3]
+        board_array[0][3] = nil
+        board = Board.new(board_array)
+
+        move = Move.new([7, 4], [5, 4])
+        player = double(color: 'white')
+
+        result = dummy_class.legal_move?(move, player, board)
+        expect(result).to be false
+      end
+    end
+  end
 
   describe '#check?' do
     context 'when called for white player with a starting board' do
