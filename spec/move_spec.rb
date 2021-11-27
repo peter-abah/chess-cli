6cfccb6751e84d7a4ad3a1f613b79017fe39da4d
Peter Abah <peterabah.ob@gmail.com>
2021-11-27 16:03:18 +0100

    Refactor test in move spec not to use @removed instance variable

--- spec/move_spec.rb
+++ spec/move_spec.rb
@@ -11,9 +11,8 @@ describe Move do
 
   describe '#initialize' do
     context 'when called with two arguments' do
-      it 'creates an object with @removed which is nil' do
-        result = move.instance_variable_get(:@removed)
-        expect(result).to be_nil
+      it 'creates an object with no removed position' do
+        expect(move.removed).to be_nil
       end
 
       it 'creates an object with @promotion as false' do
