a5c20429ef541dd5c00327a92395131ca5ac6e0d
Peter Abah <peterabah.ob@gmail.com>
2021-11-28 11:45:07 +0100

    Add test for #play_move method in HumanPlayer class

--- spec/human_player_spec.rb
+++ spec/human_player_spec.rb
@@ -1,6 +1,7 @@
 # frozen_string_literal: true
 
 require_relative '../lib/human_player'
+require_relative '../lib/board'
 
 describe HumanPlayer do
   let(:color) { 'white' }
@@ -9,6 +10,17 @@ describe HumanPlayer do
   describe '#color' do
     it 'should return the correct color' do
       expect(player.color).to eq(color)
+    end
+  end
+
+  describe '#play_move' do
+    let(:board) { Board.new }
+
+    context 'when called with a board' do
+      it 'should return a move' do
+        move = player.play_move(board)
+        expect(move).to be_a Move
+      end
     end
   end
 end
