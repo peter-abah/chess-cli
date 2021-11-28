fe0ab61e6f5f46d78d737511e09be25ed35ea792
Peter Abah <peterabah.ob@gmail.com>
2021-11-28 14:04:49 +0100

    Remove test for #play_move method in human_player spec

--- spec/human_player_spec.rb
+++ spec/human_player_spec.rb
@@ -1,7 +1,6 @@
 # frozen_string_literal: true
 
 require_relative '../lib/human_player'
-require_relative '../lib/board'
 
 describe HumanPlayer do
   let(:color) { 'white' }
@@ -10,17 +9,6 @@ describe HumanPlayer do
   describe '#color' do
     it 'should return the correct color' do
       expect(player.color).to eq(color)
-    end
-  end
-
-  describe '#play_move' do
-    let(:board) { Board.new }
-
-    context 'when called with a board' do
-      it 'should return a move' do
-        move = player.play_move(board)
-        expect(move).to be_a Move
-      end
     end
   end
 end
