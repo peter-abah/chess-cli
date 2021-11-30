a5d7101d50ee09d8abda1332cd9b7566981903a8
Peter Abah <peterabah.ob@gmail.com>
2021-11-30 09:05:24 +0100

    Refactor board spec

--- spec/board_spec.rb
+++ spec/board_spec.rb
@@ -7,170 +7,112 @@ describe Board do
     context 'when called with no argument' do
       subject(:board) { described_class.new }
 
-      it 'creates a new object with @board_array that is a array of 8 arrays' do
-        board_array = board.instance_variable_get(:@board_array)
-        result = board_array.all? { |e| e.is_a? Array }
-        expect(result).not_to be_a Array
-      end
-
-      it '@board_array element at index 1 is an array that consists of black pawns' do
-        board_array = board.instance_variable_get(:@board_array)
-        row = board_array[1]
-        result = row.all? { |p| p.is_a?(Pawn) && p.color == 'black' }
-        expect(result).to be true
-      end
-
-      it '@board_array element at index 6 is an array of white pawns' do
-        board_array = board.instance_variable_get(:@board_array)
-        row = board_array[6]
-        result = row.all? { |p| p.is_a?(Pawn) && p.color == 'white' }
-        expect(result).to be true
-      end
-
-      it 'first and last element at the first row of @board_array is a black rook' do
-        board_array = board.instance_variable_get(:@board_array)
-        row = board_array[0]
-        result = row[0].is_a?(Rook) && row[0].color == 'black' &&
-                 row[7].is_a?(Rook) && row[7].color == 'black'
-        expect(result).to be true
-      end
-
-      it 'first and last element at the last row of @board_array is a white rook' do
-        board_array = board.instance_variable_get(:@board_array)
-        row = board_array[7]
-        result = row[0].is_a?(Rook) && row[0].color == 'white' &&
-                 row[7].is_a?(Rook) && row[7].color == 'white'
-        expect(result).to be true
-      end
-
-      it '2nd and 7th elements of the 1st row of @board_array is a black knight' do
-        board_array = board.instance_variable_get(:@board_array)
-        row = board_array[0]
-        result = row[1].is_a?(Knight) && row[1].color == 'black' &&
-                 row[6].is_a?(Knight) && row[6].color == 'black'
-        expect(result).to be true
-      end
-
-      it '2nd and 7th elements of the last row of @board_array is a white knight' do
-        board_array = board.instance_variable_get(:@board_array)
-        row = board_array[7]
-        result = row[1].is_a?(Knight) && row[1].color == 'white' &&
-                 row[6].is_a?(Knight) && row[6].color == 'white'
-        expect(result).to be true
-      end
-
-      it '3rd and 6th elements of the 1st row of @board_array is black bishop' do
-        board_array = board.instance_variable_get(:@board_array)
-        row = board_array[0]
-        result = row[2].is_a?(Bishop) && row[2].color == 'black' &&
-                 row[5].is_a?(Bishop) && row[5].color == 'black'
-        expect(result).to be true
-      end
-
-      it '3rd and 6th elements of the last row of @board_array is white bishop' do
-        board_array = board.instance_variable_get(:@board_array)
-        row = board_array[7]
-        result = row[2].is_a?(Bishop) && row[2].color == 'white' &&
-                 row[5].is_a?(Bishop) && row[5].color == 'white'
-        expect(result).to be true
-      end
-
-      it '4th element is black queen and 5th element is a black king in the 1st row of @board_array' do
-        board_array = board.instance_variable_get(:@board_array)
-        row = board_array[0]
-        result = row[3].is_a?(Queen) && row[3].color == 'black' &&
-                 row[4].is_a?(King) && row[4].color == 'black'
-        expect(result).to be true
-      end
-
-      it '4th element is white queen and 5th element is a white king in the last row of @board_array' do
-        board_array = board.instance_variable_get(:@board_array)
-        row = board_array[7]
-        result = row[3].is_a?(Queen) && row[3].color == 'white' &&
-                 row[4].is_a?(King) && row[4].color == 'white'
-        expect(result).to be true
-      end
-
-      it 'the rest rows are empty arrays' do
-        board_array = board.instance_variable_get(:@board_array)
-        result = board_array[2..5].all? do |e|
-          e.reject(&:nil?).empty?
+      matcher :have_pawns_at_default_positions do |color|
+        match do |board|
+          y = color == 'white' ? 6 : 1
+          pawns = (0..7).map { |x| board.piece_at(y: y, x: x) }
+          pawns.all? { |pawn| pawn.color == color && pawn.is_a?(Pawn) }
         end
-
-        expect(result).to be true
       end
 
-      it 'saves pieces positions in @pieces' do
-        # TO DO
+      matcher :have_knights_at_default_positions do |color|
+        match do |board|
+          y = color == 'white' ? 7 : 0
+          knights = [1, 6].map { |x| board.piece_at(y: y, x: x) }
+          knights.all? { |knight| knight.color == color && knight.is_a?(Knight) }
+        end
+      end
+
+      matcher :have_knights_at_default_positions do |color|
+        match do |board|
+          y = color == 'white' ? 7 : 0
+          knights = [1, 6].map { |x| board.piece_at(y: y, x: x) }
+          knights.all? { |knight| knight.color == color && knight.is_a?(Knight) }
+        end
+      end
+
+      matcher :have_rooks_at_default_positions do |color|
+        match do |board|
+          y = color == 'white' ? 7 : 0
+          rooks = [0, 7].map { |x| board.piece_at(y: y, x: x) }
+          rooks.all? { |rook| rook.color == color && rook.is_a?(Rook) }
+        end
+      end
+
+      matcher :have_bishops_at_default_positions do |color|
+        match do |board|
+          y = color == 'white' ? 7 : 0
+          bishops = [2, 5].map { |x| board.piece_at(y: y, x: x) }
+          bishops.all? { |bishop| bishop.color == color && bishop.is_a?(Bishop) }
+        end
+      end
+
+      matcher :have_king_at_default_position do |color|
+        match do |board|
+          y = color == 'white' ? 7 : 0
+          x = 4
+          king = board.piece_at(y: y, x: x)
+          king.color == color && king.is_a?(King)
+        end
+      end
+
+      matcher :have_queen_at_default_position do |color|
+        match do |board|
+          y = color == 'white' ? 7 : 0
+          x = 3
+          queen = board.piece_at(y: y, x: x)
+          queen.color == color && queen.is_a?(Queen)
+        end
+      end
+
+      it 'creates a new board with pawns at default positions' do
+        expect(board).to have_pawns_at_default_positions 'white'
+        expect(board).to have_pawns_at_default_positions 'black'
+      end
+
+      it 'creates a new board with knights at default positions' do
+        expect(board).to have_knights_at_default_positions 'white'
+        expect(board).to have_knights_at_default_positions 'black'
+      end
+
+      it 'creates a new board with rooks at default positions' do
+        expect(board).to have_rooks_at_default_positions 'white'
+        expect(board).to have_rooks_at_default_positions 'black'
+      end
+
+      it 'creates a new board with bishops at default positions' do
+        expect(board).to have_bishops_at_default_positions 'white'
+        expect(board).to have_bishops_at_default_positions 'black'
+      end
+
+      it 'creates a new board with kings at default positions' do
+        expect(board).to have_king_at_default_position 'white'
+        expect(board).to have_king_at_default_position 'black'
+      end
+
+      it 'creates a new board with queens at default positions' do
+        expect(board).to have_queen_at_default_position 'white'
+        expect(board).to have_queen_at_default_position 'black'
       end
     end
 
-    context 'when called with two arguments' do
-      let(:board_array) { 'first' }
-      let(:prev_board_array) { 'second' }
-      subject(:board) { described_class.new(board_array, prev_board_array) }
+    context 'when called with a fen notation' do
+      let(:fen_notation) { 'P7/4r3/8/8/6k1/8/8/8' }
+      subject(:board) { described_class.new(fen_notation: fen_notation) }
 
-      xit 'creates a new object with @board_array as first argument' do
-        result = board.instance_variable_get(:@board_array)
-        expect(result).to eq(board_array)
+      it 'creates a new board corresponding to the fen notation' do
+        white_pawn = board.piece_at(y: 0, x: 0)
+        expect(white_pawn).to be_a(Pawn).and have_attributes(color: 'white')
       end
 
-      xit 'creates a new object with @prev_board_array as 2nd argument' do
-        result = board.instance_variable_get(:@prev_board_array)
-        expect(result).to eq(prev_board_array)
-      end
-    end
-  end
-
-  describe '#update' do
-    subject(:board) { described_class.new }
-
-    context 'when called with a move to move a pawn' do
-      it 'returns a new board updated with the move' do
-        move = double(moved: { [1, 3] => [3, 3] }, removed: nil, promotion: false)
-
-        new_board_array = board.update(move).instance_variable_get(:@board_array)
-        result = new_board_array[3][3]
-
-        expect(result).to be_a Pawn
+      it 'creates a new board corresponding to the fen notation' do
+        black_rook = board.piece_at(y: 1, x: 4)
+        expect(black_rook).to be_a(Rook).and have_attributes(color: 'black')
       end
 
-      it 'returns a new board with @prev_board_array as the board board_array' do
-        board_array = board.instance_variable_get(:@board_array)
-        move = double(moved: { [1, 3] => [3, 3] }, removed: nil, promotion: false)
-
-        prev_board_array = board.update(move).instance_variable_get(:@prev_board_array)
-        expect(prev_board_array).to eq(board_array)
-      end
-    end
-
-    context 'when called with a move to move a knight' do
-      it 'returns a new board updated with the move' do
-        move = double(moved: { [0, 1] => [2, 0] }, removed: nil, promotion: false)
-
-        new_board_array = board.update(move).instance_variable_get(:@board_array)
-        result = new_board_array[2][0]
-        expect(result).to be_a Knight
-      end
-    end
-
-    context 'when called with a move to remove a piece' do
-      it 'returns a new board updated with the board' do
-        move = double(moved: { [0, 1] => [2, 0] }, removed: [7, 5], promotion: false)
-
-        new_board_array = board.update(move).instance_variable_get(:@board_array)
-        result = new_board_array[7][5]
-        expect(result).to be_nil
-      end
-    end
-
-    context 'when called with a move that promotes a pawn' do
-      it 'returns a new board with the piece being promoted' do
-        move = double(moved: { [1, 1] => [2, 1] }, removed: nil, promotion: Queen)
-
-        new_board_array = board.update(move).instance_variable_get(:@board_array)
-        result = new_board_array[2][1]
-        expect(result).to be_a Queen
+      it 'creates a new board corresponding to the fen notation' do
+        black_king = board.piece_at(y: 4, x: 6)
+        expect(black_king).to be_a(King).and have_attributes(color: 'black')
       end
     end
   end
@@ -187,6 +129,52 @@ describe Board do
       it 'should return the correct piece at y and x position' do
         piece = board.piece_at(y: 0, x: 0)
         expect(piece).to be_a Rook
+      end
+    end
+  end
+
+  describe '#update' do
+    subject(:board) { described_class.new }
+
+    context 'when called with a move to move a pawn' do
+      it 'returns a new board updated with the move' do
+        move = double(moved: { [1, 3] => [3, 3] }, removed: nil, promotion: false)
+        piece = board.update(move).piece_at(y: 3, x: 3)
+        expect(piece).to be_a Pawn
+      end
+
+      # TODO: Make it test prev_state
+      it 'returns a new board with @prev_board_array as the board board_array' do
+        board_array = board.instance_variable_get(:@board_array)
+        move = double(moved: { [1, 3] => [3, 3] }, removed: nil, promotion: false)
+
+        prev_board_array = board.update(move).instance_variable_get(:@prev_board_array)
+        expect(prev_board_array).to eq(board_array)
+      end
+    end
+
+    context 'when called with a move to move a knight' do
+      it 'returns a new board updated with the move' do
+        move = double(moved: { [0, 1] => [2, 0] }, removed: nil, promotion: false)
+        piece = board.update(move).piece_at(y: 2, x: 0)
+        expect(piece).to be_a Knight
+      end
+    end
+
+    context 'when called with a move to remove a piece' do
+      it 'returns a new board updated with the board' do
+        move = double(moved: { [0, 1] => [2, 0] }, removed: [7, 5], promotion: false)
+        piece = board.update(move).piece_at(y: 7, x: 5)
+        expect(piece).to be_nil
+      end
+    end
+
+    context 'when called with a move that promotes a pawn' do
+      it 'returns a new board with the piece being promoted' do
+        move = double(moved: { [1, 1] => [2, 1] }, removed: nil, promotion: Queen)
+
+        piece = board.update(move).piece_at(y: 2, x: 1)
+        expect(piece).to be_a Queen
       end
     end
   end
