df158274e00f449111094b8a9ce7bb04740429cd
Peter Abah <peterabah.ob@gmail.com>
2021-12-05 07:54:58 +0100

    Add more methods to ConsoleUI class

--- lib/console_ui.rb
+++ lib/console_ui.rb
@@ -2,28 +2,84 @@
 
 require 'rocketman'
 require_relative './game'
+require_relative './prompts'
+require_relative './serializer'
+require_relative './display'
+require_rel './players'
 
 # Class for interface to play chess on the console
 class ConsoleUI
   include Rocketman::Producer
   extend Rocketman::Consumer
+  PLAYER_COLORS = %w[white black].freeze
 
   def initialize
+    puts Prompts.welcome
+
     @game, @players = init_game
+    @current_player = @players[:white]
     @game.start
   end
 
   def init_game
-    # Display instructions
-    # Get player option for new game or to load game
-    # If new game create new game and load players
-    # else load saved game and return
+    game_option = get_game_option
+    case game_option
+    when 1
+      new_game
+    when 2
+      load_game
+    end
+  end
+
+  def new_game
+    [Game.new, init_players]
+  end
+
+  def init_players
+    PLAYER_COLORS.reduce({}) do |players, color|
+      players[color.to_sym] = player_type(color)
+    end
+  end
+
+  def player_type(color)
+    choice = nil
+    loop do
+      puts Prompts.player_type(color)
+      choice = gets.chomp.to_i
+      break if [1, 2].include?(choice)
+    end
+    choice == 1 ? Player.new(color) : ComputerPlayer.new(color)
+  end
+
+  def load_game
+    saved_games = Serializer.saved_games
+
+    if saved_games.empty?
+      puts Prompts.no_saved_games
+      return
+    end
+
+    saved_game = saved_games_choice(saved_games)
+    Serialize.load_game(saved_game)
+  end
+
+  def saved_game_choice(saved_games)
+    choice = -1
+    until choice.between?(0, saved_games.length)
+      puts Prompts.saved_games_choice(saved_games)
+      choice = gets.chomp.to_i
+    end
+    saved_games[choice - 1]
   end
 
   def display_board(board)
+    puts Display.board_representation(board)
   end
 
   def player_move(payload)
+    player = @players[payload.color.to_sym]
+    move = player.is_a? ComputerPlayer ? player.move : prompt_move
+    # emit move to game
   end
 
   def end_game(payload)
