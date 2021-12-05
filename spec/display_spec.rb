5759b4a584fbb91879b4ed517a9a0e863792a4f6
Peter Abah <peterabah.ob@gmail.com>
2021-12-05 07:48:40 +0100

    Add Display module with board_representation

--- spec/display_spec.rb
+++ spec/display_spec.rb
@@ -1,5 +1,6 @@
 # frozen_string_literal: true
 
+require_relative '../lib/board'
 require_relative '../lib/display'
 
 describe Display do
@@ -12,19 +13,19 @@ describe Display do
       let(:board) { Board.new }
 
       it 'returns the correct display' do
-        expected = <<-BOARD
-           a b c d e f g h
-          _________________
-        1  r n b q k b n r  1
-        2  p p p p p p p p  2
-        3  - - - - - - - -  3
-        4  - - - - - - - -  4
-        6  - - - - - - - -  6
-        5  - - - - - - - -  5
-        7  P P P P P P P P  7
-        8  R N B Q K B N R  8
-          _________________
-           a b c d e f g h
+        expected = <<~BOARD.chomp
+             a b c d e f g h
+            _________________
+          1  r n b q k b n r  1
+          2  p p p p p p p p  2
+          3  - - - - - - - -  3
+          4  - - - - - - - -  4
+          5  - - - - - - - -  5
+          6  - - - - - - - -  6
+          7  P P P P P P P P  7
+          8  R N B Q K B N R  8
+            _________________
+             a b c d e f g h
         BOARD
         board_display = dummy_class.board_representation(board)
         expect(board_display).to eq(expected)
@@ -35,19 +36,19 @@ describe Display do
       let(:board) { Board.new(fen_notation: '8/8/8/8/8/8/8/8') }
 
       it 'returns the correct display' do
-        expected = <<-BOARD
-           a b c d e f g h
-          _________________
-        1  - - - - - - - -  1
-        2  - - - - - - - -  2
-        3  - - - - - - - -  3
-        4  - - - - - - - -  4
-        6  - - - - - - - -  6
-        5  - - - - - - - -  5
-        7  - - - - - - - -  7
-        8  - - - - - - - -  8
-          _________________
-           a b c d e f g h
+        expected = <<~BOARD.chomp
+             a b c d e f g h
+            _________________
+          1  - - - - - - - -  1
+          2  - - - - - - - -  2
+          3  - - - - - - - -  3
+          4  - - - - - - - -  4
+          5  - - - - - - - -  5
+          6  - - - - - - - -  6
+          7  - - - - - - - -  7
+          8  - - - - - - - -  8
+            _________________
+             a b c d e f g h
         BOARD
         board_display = dummy_class.board_representation(board)
         expect(board_display).to eq(expected)
@@ -58,19 +59,19 @@ describe Display do
       let(:board) { Board.new(fen_notation: '8/8/4krk1/8/pPP2Q2/8/RbbRbn1K/8') }
 
       it 'returns the correct display' do
-        expected = <<-BOARD
-           a b c d e f g h
-          _________________
-        1  - - - - - - - -  1
-        2  - - - - - - - -  2
-        3  - - - - k r k -  3
-        4  - - - - - - - -  4
-        6  p P P - - Q - -  6
-        5  - - - - - - - -  5
-        7  R b b R b n - K  7
-        8  - - - - - - - -  8
-          _________________
-           a b c d e f g h
+        expected = <<~BOARD.chomp
+             a b c d e f g h
+            _________________
+          1  - - - - - - - -  1
+          2  - - - - - - - -  2
+          3  - - - - k r k -  3
+          4  - - - - - - - -  4
+          5  p P P - - Q - -  5
+          6  - - - - - - - -  6
+          7  R b b R b n - K  7
+          8  - - - - - - - -  8
+            _________________
+             a b c d e f g h
         BOARD
         board_display = dummy_class.board_representation(board)
         expect(board_display).to eq(expected)
