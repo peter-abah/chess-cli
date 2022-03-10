ffc009268e4b09b167870396191e1d3adf1cb3d0
Peter Abah <peterabah.ob@gmail.com>
2022-03-10 16:20:55 +0100

    Add tests for #piece_at and #pieces method for board

---
+++ spec/board_matchers.rb
@@ -0,0 +1,83 @@
+# frozen_string_literal: true
+
+require 'pry-byebug'
+require 'rspec/expectations'
+require 'require_all'
+
+require_rel '../lib/pieces'
+require_relative '../lib/position'
+
+RSpec::Matchers.define :have_all_pieces_at_default_positions do
+  match do |pieces|
+    has_rooks?(pieces) && has_knights?(pieces) && has_bishops?(pieces) &&
+    has_queens?(pieces) && has_kings?(pieces) && has_pawns?(pieces) &&
+    pieces.size == 32
+  end
+  
+  def has_rooks?(pieces)
+    has_piece_of_type?(
+      pieces: pieces, klass: Rook, positions: ['a1', 'h1'], color: :white
+    )
+    has_piece_of_type?(
+      pieces: pieces, klass: Rook, positions: ['a8', 'h8'], color: :black
+    )
+  end
+  
+  def has_bishops?(pieces)
+    has_piece_of_type?(
+      pieces: pieces, klass: Bishop, positions: ['c1', 'f1'], color: :white
+    )
+    has_piece_of_type?(
+      pieces: pieces, klass: Bishop, positions: ['c8', 'f8'], color: :black
+    )
+  end
+  
+  def has_knights?(pieces)
+    has_piece_of_type?(
+      pieces: pieces, klass: Knight, positions: ['b1', 'g1'], color: :white
+    )
+    has_piece_of_type?(
+      pieces: pieces, klass: Knight, positions: ['b8', 'g8'], color: :black
+    )
+  end
+  
+  def has_queens?(pieces)
+    has_piece_of_type?(
+      pieces: pieces, klass: Queen, positions: ['d1'], color: :white
+    )
+    has_piece_of_type?(
+      pieces: pieces, klass: Queen, positions: ['d8'], color: :black
+    )
+  end
+  
+  def has_kings?(pieces)
+    has_piece_of_type?(
+      pieces: pieces, klass: King, positions: ['e1'], color: :white
+    )
+    has_piece_of_type?(
+      pieces: pieces, klass: King, positions: ['e8'], color: :black
+    )
+  end
+  
+  def has_pawns?(pieces)
+    white_pawn_positions = ('a'..'h').map { |letter| "#{letter}2" }
+    black_pawn_positions = ('a'..'h').map { |letter| "#{letter}7" }
+
+    has_piece_of_type?(
+      pieces: pieces, klass: Pawn, positions: white_pawn_positions, color: :white
+    )
+    has_piece_of_type?(
+      pieces: pieces, klass: Pawn, positions: black_pawn_positions, color: :black
+    )
+  end
+  
+  def has_piece_of_type?(pieces:, klass:, positions:, color:)
+    positions.all? do |pos|
+      pieces.any? do |piece|
+        piece.color == color && 
+        piece.position == Position.parse(pos) &&
+        piece.is_a?(klass)
+      end
+    end
+  end
+end
--- spec/board_spec.rb
+++ spec/board_spec.rb
@@ -1,180 +1,65 @@
 # frozen_string_literal: true
 
+require 'require_all'
+
+require_relative 'board_matchers'
 require_relative '../lib/board'
+require_relative '../lib/position'
+require_rel '../lib/pieces'
 
 describe Board do
-  describe '#initilize' do
-    context 'when called with no argument' do
-      subject(:board) { described_class.new }
-
-      matcher :have_pawns_at_default_positions do |color|
-        match do |board|
-          y = color == 'white' ? 6 : 1
-          pawns = (0..7).map { |x| board.piece_at(y: y, x: x) }
-          pawns.all? { |pawn| pawn.color == color && pawn.is_a?(Pawn) }
-        end
-      end
-
-      matcher :have_knights_at_default_positions do |color|
-        match do |board|
-          y = color == 'white' ? 7 : 0
-          knights = [1, 6].map { |x| board.piece_at(y: y, x: x) }
-          knights.all? { |knight| knight.color == color && knight.is_a?(Knight) }
-        end
-      end
-
-      matcher :have_knights_at_default_positions do |color|
-        match do |board|
-          y = color == 'white' ? 7 : 0
-          knights = [1, 6].map { |x| board.piece_at(y: y, x: x) }
-          knights.all? { |knight| knight.color == color && knight.is_a?(Knight) }
-        end
-      end
-
-      matcher :have_rooks_at_default_positions do |color|
-        match do |board|
-          y = color == 'white' ? 7 : 0
-          rooks = [0, 7].map { |x| board.piece_at(y: y, x: x) }
-          rooks.all? { |rook| rook.color == color && rook.is_a?(Rook) }
-        end
-      end
-
-      matcher :have_bishops_at_default_positions do |color|
-        match do |board|
-          y = color == 'white' ? 7 : 0
-          bishops = [2, 5].map { |x| board.piece_at(y: y, x: x) }
-          bishops.all? { |bishop| bishop.color == color && bishop.is_a?(Bishop) }
-        end
-      end
-
-      matcher :have_king_at_default_position do |color|
-        match do |board|
-          y = color == 'white' ? 7 : 0
-          x = 4
-          king = board.piece_at(y: y, x: x)
-          king.color == color && king.is_a?(King)
-        end
-      end
-
-      matcher :have_queen_at_default_position do |color|
-        match do |board|
-          y = color == 'white' ? 7 : 0
-          x = 3
-          queen = board.piece_at(y: y, x: x)
-          queen.color == color && queen.is_a?(Queen)
-        end
-      end
-
-      it 'creates a new board with pawns at default positions' do
-        expect(board).to have_pawns_at_default_positions 'white'
-        expect(board).to have_pawns_at_default_positions 'black'
-      end
-
-      it 'creates a new board with knights at default positions' do
-        expect(board).to have_knights_at_default_positions 'white'
-        expect(board).to have_knights_at_default_positions 'black'
-      end
-
-      it 'creates a new board with rooks at default positions' do
-        expect(board).to have_rooks_at_default_positions 'white'
-        expect(board).to have_rooks_at_default_positions 'black'
-      end
-
-      it 'creates a new board with bishops at default positions' do
-        expect(board).to have_bishops_at_default_positions 'white'
-        expect(board).to have_bishops_at_default_positions 'black'
-      end
-
-      it 'creates a new board with kings at default positions' do
-        expect(board).to have_king_at_default_position 'white'
-        expect(board).to have_king_at_default_position 'black'
-      end
-
-      it 'creates a new board with queens at default positions' do
-        expect(board).to have_queen_at_default_position 'white'
-        expect(board).to have_queen_at_default_position 'black'
-      end
-    end
-
-    context 'when called with a fen notation' do
-      let(:fen_notation) { 'P7/4r3/8/8/6k1/8/8/8' }
-      subject(:board) { described_class.new(fen_notation: fen_notation) }
-
-      it 'creates a new board corresponding to the fen notation' do
-        white_pawn = board.piece_at(y: 0, x: 0)
-        expect(white_pawn).to be_a(Pawn).and have_attributes(color: 'white')
-      end
-
-      it 'creates a new board corresponding to the fen notation' do
-        black_rook = board.piece_at(y: 1, x: 4)
-        expect(black_rook).to be_a(Rook).and have_attributes(color: 'black')
-      end
-
-      it 'creates a new board corresponding to the fen notation' do
-        black_king = board.piece_at(y: 4, x: 6)
-        expect(black_king).to be_a(King).and have_attributes(color: 'black')
-      end
-    end
-  end
+  subject(:board) { described_class.new }
 
   describe '#piece_at' do
-    subject(:board) { described_class.new }
-
-    context 'when called with y and x' do
-      it 'should return a piece' do
-        piece = board.piece_at(y: 0, x: 0)
-        expect(piece).to be_a Piece
-      end
-
-      it 'should return the correct piece at y and x position' do
-        piece = board.piece_at(y: 0, x: 0)
-        expect(piece).to be_a Rook
+    context 'when called with position a2' do
+      it 'should return a white Pawn' do
+        piece = board.piece_at(Position.parse('a2'))
+        expect(piece).to be_a(Pawn).and have_attributes(color: :white)
       end
     end
-  end
-
-  describe '#update' do
-    subject(:board) { described_class.new }
-
-    context 'when called with a move to move a pawn' do
-      it 'returns a new board updated with the move' do
-        move = double(moved: { [1, 3] => [3, 3] }, removed: nil, promotion: false)
-        piece = board.update(move).piece_at(y: 3, x: 3)
-        expect(piece).to be_a Pawn
-      end
-
-      # TODO: Make it test prev_state
-      it 'returns a new board with @prev_board_array as the board board_array' do
-        board_array = board.instance_variable_get(:@board_array)
-        move = double(moved: { [1, 3] => [3, 3] }, removed: nil, promotion: false)
-
-        prev_board_array = board.update(move).instance_variable_get(:@prev_board_array)
-        expect(prev_board_array).to eq(board_array)
+    
+    context 'when called with position e8' do
+      it 'should return a black King' do
+        piece = board.piece_at(Position.parse('e8'))
+        expect(piece).to be_a(King).and have_attributes(color: :black)
       end
     end
-
-    context 'when called with a move to move a knight' do
-      it 'returns a new board updated with the move' do
-        move = double(moved: { [0, 1] => [2, 0] }, removed: nil, promotion: false)
-        piece = board.update(move).piece_at(y: 2, x: 0)
-        expect(piece).to be_a Knight
-      end
-    end
-
-    context 'when called with a move to remove a piece' do
-      it 'returns a new board updated with the board' do
-        move = double(moved: { [0, 1] => [2, 0] }, removed: [7, 5], promotion: false)
-        piece = board.update(move).piece_at(y: 7, x: 5)
+    
+    context 'when called with position h4' do
+      it 'should return nil' do
+        piece = board.piece_at(Position.parse('h4'))
         expect(piece).to be_nil
       end
     end
+  end
+  
+  describe '#pieces' do
+    context 'when board is initialized without arguments' do
+      let(:board) { described_class.new }
 
-    context 'when called with a move that promotes a pawn' do
-      it 'returns a new board with the piece being promoted' do
-        move = double(moved: { [1, 1] => [2, 1] }, removed: nil, promotion: Queen)
-
-        piece = board.update(move).piece_at(y: 2, x: 1)
-        expect(piece).to be_a Queen
+      it 'returns pieces for a starting chess board at their default positions' do
+        expect(board.pieces).to have_all_pieces_at_default_positions
+      end
+    end
+    
+    context 'when board is initialized with a segments hash' do
+      segments = { pieces: [Rook.new(:white, Position.parse('a3'))] }
+      let(:board) { described_class.new(segments: segments) }
+ 
+      it 'returns the pieces set in the segments hash' do
+        expect(board.pieces).to contain_exactly(
+          be_a(Rook).and have_attributes(color: :white, position: Position.parse('a3'))
+        )
+      end
+    end
+    
+    context 'when board is initialized with fen notation' do
+      let(:board) { described_class.new(fen_notation: '8/8/8/8/8/R7/8/8 w - - 0 1') }
+ 
+      it 'returns the pieces set in the segments hash' do
+        expect(board.pieces).to contain_exactly(
+          be_a(Rook).and have_attributes(color: :white, position: Position.parse('a3'))
+        )
       end
     end
   end
