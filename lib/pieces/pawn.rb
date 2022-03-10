f627987c16e6420b7bbeb7a0ff5307aa9f676544
Peter Abah <peterabah.ob@gmail.com>
2022-03-10 11:58:52 +0100

    Add move_set for pawn capture move and methods for promotion

--- lib/pieces/pawn.rb
+++ lib/pieces/pawn.rb
@@ -11,114 +11,33 @@ class Pawn < Piece
     super
 
     # where the pawn is facing -1 for up, 1 for down
-    direction = color == :white ? -1 : 1
-    increments = [{ y: direction, x: 0 }]
-    repeat = has_moved ? 1 : 2
-    @move_sets = [MoveSet.new(
-      increments: increments,
-      repeat: repeat,
-      blocked_by: :all,
-      special_moves: %i[en_passant promotion]
-    ), MoveSet.new(repeat: 1, increments: [], blocked_by: :player_piece, special_moves: [:pawn_capture])]
+    @direction = color == :white ? -1 : 1
+    repeat = position.starting_pawn_rank?(color) ? 2 : 1
+    @move_sets = [
+      # for normal move
+      MoveSet.new(
+        increments: [{ y: direction, x: 0 }],
+        repeat: repeat,
+        blocked_by: [:piece],
+        special_moves: %i[en_passant],
+        promotable: true
+      ),
+      # for capture
+      MoveSet.new(
+        repeat: 1,
+        increments: [{ y: direction, x: 1 }, { y: direction, x: -1}],
+        blocked_by: [:same, :empty],
+        promotable: true,
+      )
+    ]
   end
-
-  # def possible_moves(board, pos)
-  #   y, x = pos
-  #   piece = board.piece_at(y: y, x: x)
-  #   raise error unless piece == self
-
-  #   moves = [
-  #     normal_moves(board, pos),
-  #     capture_moves(board, pos),
-  #     en_passant_moves(board, pos)
-  #   ]
-  #   moves.reduce { |total, m| total + m }
-  # end
-
-  # private
-
-  # def normal_moves(board, pos)
-  #   result = [forward_move(board, pos, direction)]
-  #   result.push(forward_move(board, pos, direction * 2)) if double_move_possible?(board, pos)
-  #   result.reject(&:nil?)
-  # end
-
-  # def forward_move(board, pos, y_amount)
-  #   y, x = pos
-  #   yn = y + y_amount
-  #   return unless forward_move_possible?(board, [yn, x])
-
-  #   move = Move.new(pos, [yn, x])
-  #   move.promotion = true if yn.zero? || yn == 7
-  #   move
-  # end
-
-  # def forward_move_possible?(board, (y, x))
-  #   y > 7 || board.piece_at(y: y, x: x).nil?
-  # end
-
-  # def double_move_possible?(board, (y, x))
-  #   y == start_pos && board.piece_at(y: y + direction, x: x).nil?
-  # end
-
-  # def capture_moves(board, pos)
-  #   y, x = pos
-  #   yn = y + direction
-  #   x_left = x - 1
-  #   x_right = x + 1
-
-  #   moves = [
-  #     capture_move(board, pos, [yn, x_left]),
-  #     capture_move(board, pos, [yn, x_right])
-  #   ]
-  #   moves.reject(&:nil?)
-  # end
-
-  # def capture_move(board, pos, new_pos)
-  #   yn, = new_pos
-  #   return unless capture_move_possible?(board, new_pos)
-
-  #   move = Move.new(pos, new_pos, new_pos)
-  #   move.promotion = true if yn.zero? || yn == 7
-  #   move
-  # end
-
-  # def capture_move_possible?(board, (y, x))
-  #   return false unless x.between?(0, 7)
-
-  #   piece = board.piece_at(y: y, x: x)
-  #   !(piece.nil? || piece.color == color)
-  # end
-
-  # def en_passant_moves(board, pos)
-  #   y, x = pos
-  #   x_left = x - 1
-  #   x_right = x + 1
-  #   return [] unless en_passant_rank?(y)
-
-  #   moves = [
-  #     en_passant_move(board, pos, x_left),
-  #     en_passant_move(board, pos, x_right)
-  #   ]
-  #   moves.reject(&:nil?)
-  # end
-
-  # def en_passant_rank?(y_pos)
-  #   y_pos == start_pos + (direction * 3)
-  # end
-
-
-  # def en_passant_move(board, pos, xn)
-  #   y, = pos
-  #   piece = xn.between?(0, 7) ? board.piece_at(y: y, x: xn) : nil
-  #   return unless piece.is_a?(Pawn) && piece.color != color
-
-  #   prev_state = board.prev_state
-  #   yn = y + (direction * 2)
-  #   return unless prev_state.piece_at(y: yn, x: xn).is_a? Pawn
-
-  #   move = Move.new(pos, [y + direction, xn], [y, xn])
-  #   move.en_passant = true
-  #   move
-  # end
+  
+  def can_promote?
+    rank = color == :white ? 1  : 6
+    position.y == rank
+  end
+  
+  def promotion_pieces
+    [:Knight, :Rook, :Bishop, :Queen]
+  end
 end
--- spec/pieces/pawn_spec.rb
+++ spec/pieces/pawn_spec.rb
@@ -4,49 +4,119 @@ require_relative '../../lib/pieces/pawn'
 require_relative './piece_spec'
 
 describe Pawn do
-  let(:position) { Position.new(y: 1, x: 1) }
+  let(:position) { Position.parse('a2') }
   subject(:pawn) { described_class.new(:white, position) }
   
   it_behaves_like 'a chess piece', described_class
 
-  xdescribe '#direction' do
-    context 'when called color is white' do
-      let(:color) { 'white' }
-      subject(:pawn) { described_class.new(color) }
+  describe '#direction' do
+    context 'when color is white' do
+      subject(:pawn) { described_class.new(:white, position) }
+      
+      it 'returns -1' do
+        expect(pawn.direction).to eq -1
+      end
+    end
+    
+    context 'when color is black' do
+      subject(:pawn) { described_class.new(:black, position) }
+      
+      it 'returns 1' do
+        expect(pawn.direction).to eq 1
+      end
     end
   end
 
   describe '#move_sets' do
-    it 'returns the correct move_sets' do
-      move_set, = pawn.move_sets
+    it 'has a size of 2 elements' do
+      expect(pawn.move_sets.size).to eq 2
+    end
+    
+    it 'the first moveset is for normal moves' do
+      move_set = pawn.move_sets.first
       expected_increments = [{ y: -1, x: 0 }]
 
-      expect(pawn.move_sets.size).to eq 2
       expect(move_set.increments).to eq expected_increments
       expect(move_set.repeat).to eq 2
-      expect(move_set.blocked_by).to eq :all
-      expect(move_set.special_moves).to eq %i[en_passant promotion]
+      expect(move_set.blocked_by).to eq [:piece]
+      expect(move_set.special_moves).to eq %i[en_passant]
+      expect(move_set.promotable).to be true
+    end
+    
+    it 'the second moveset is for capture moves' do
+      move_set = pawn.move_sets.last
+      expected_increments = [{ y: -1, x: 1 }, { y: -1, x: -1}]
+
+      expect(move_set.increments).to eq expected_increments
+      expect(move_set.repeat).to eq 1
+      expect(move_set.blocked_by).to contain_exactly(:empty, :same)
+      expect(move_set.special_moves).to be_empty
+      expect(move_set.promotable).to be true
     end
 
-    context 'when color is different' do
+    context 'when color is black' do
       subject(:pawn) { described_class.new(:black, position) }
 
-      it 'returns move_set with different increments' do
-        move_set, = pawn.move_sets
+      it 'the y value for normal move increments is 1' do
+        move_set = pawn.move_sets.first
         expected_increments = [{ y: 1, x: 0 }]
+        expect(move_set.increments).to eq expected_increments
+      end
+      
+      it 'the y value for capture move increments is 1' do
+        move_set = pawn.move_sets.last
+        expected_increments = [{ y: 1, x: 1 }, { y: 1, x: -1}]
         expect(move_set.increments).to eq expected_increments
       end
     end
 
-    xcontext 'when the pawn has moved' do
-      subject(:pawn) { described_class.new(:white) }
+    context 'when the pawn position is not starting rank' do
+      subject(:pawn) { described_class.new(:white, Position.parse('a4')) }
 
-      before { pawn.has_moved = true }
-
-      it 'returns move_set with repeat as 1' do
-        move_set, = pawn.move_sets
+      it 'the normal move_set has repeat as 1' do
+        move_set = pawn.move_sets.first
         expect(move_set.repeat).to eq 1
       end
+    end
+  end
+  
+  describe '#can_promote?' do
+    context 'when pawn color is white and position is at rank 7' do
+      let(:pawn) { described_class.new(:white, Position.parse('a7')) }
+      
+      it 'returns true' do
+        expect(pawn.can_promote?).to be true
+      end
+    end
+    
+    context 'when pawn color is white and position is not at rank 7' do
+      let(:pawn) { described_class.new(:white, Position.parse('a5')) }
+      
+      it 'returns false' do
+        expect(pawn.can_promote?).to be false
+      end
+    end
+    
+    context 'when pawn color is black and position is at rank 2' do
+      let(:pawn) { described_class.new(:black, Position.parse('a2')) }
+      
+      it 'returns true' do
+        expect(pawn.can_promote?).to be true
+      end
+    end
+    
+    context 'when pawn color is black and position is not at rank 2' do
+      let(:pawn) { described_class.new(:black, Position.parse('a7')) }
+      
+      it 'returns false' do
+        expect(pawn.can_promote?).to be false
+      end
+    end
+  end
+  
+  describe '#promotion_pieces' do
+    it 'returns the correct promotion pieces for pawn' do
+      expect(pawn.promotion_pieces).to contain_exactly(:Queen, :Knight, :Rook, :Bishop)
     end
   end
 
