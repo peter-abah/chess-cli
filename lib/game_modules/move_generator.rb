2fabe7e13420815e97f6960c433d66147a9647d2
Peter Abah <peterabah.ob@gmail.com>
2022-03-14 11:35:19 +0100

    Change move generator to accept board as argument

--- lib/game_modules/move_generator.rb
+++ lib/game_modules/move_generator.rb
@@ -12,35 +12,37 @@ module MoveGenerator
     castle: :castle_moves,
     en_passant: :en_passant_moves,
   }.freeze
-  
-  PROMOTION_PIECES = [:Knight, :Bishop, :Rook, :Queen].freeze
 
-  def moves_for_pos(pos)
+  def moves_for_pos(pos, board)
     piece = board.piece_at pos
     return [] unless piece&.color == board.active_color
 
-    moves_for_piece(piece)
+    moves_for_piece(piece, board)
   end
 
-  def moves_for_piece(piece)
+  def moves_for_piece(piece, board)
     piece.move_sets.reduce([]) do |result, move_set|
       move_set.increments.each do |increment|
-        result.concat gen_moves_for_increment(move_set, increment, piece)
+        result.concat(
+          gen_moves_for_increment(board, move_set, increment, piece)
+        )
       end
 
-      result.concat gen_special_moves(move_set, piece)
+      result.concat gen_special_moves(board, move_set, piece)
     end
   end
   
   private
   
-  def gen_moves_for_increment(move_set, increment, piece)
+  def gen_moves_for_increment(board, move_set, increment, piece)
     x, y = increment.values_at(:x, :y)
     result = []
 
     1.upto(move_set.repeat) do |factor|
       new_pos = piece.position.increment(y: y * factor, x: x * factor)
-      break if new_pos.out_of_bounds? || blocked?(move_set, piece, new_pos)
+      if new_pos.out_of_bounds? || blocked?(board, move_set, piece, new_pos)
+        break
+      end
       
       removed = board.piece_at(new_pos).nil? ? nil : new_pos.to_s
       from = piece.position.to_s
@@ -64,7 +66,7 @@ module MoveGenerator
     end
   end
   
-  def blocked?(move_set, piece, new_pos)
+  def blocked?(board, move_set, piece, new_pos)
     other_piece = board.piece_at new_pos
     return true if other_piece.nil? && move_set.blocked_by.include?(:empty)
     
@@ -77,20 +79,23 @@ module MoveGenerator
     false
   end
   
-  def gen_special_moves(move_set, piece)
+  def gen_special_moves(board, move_set, piece)
     result = []
     move_set.special_moves.each do |sym|
-      result.concat self.send(SPECIAL_MOVES[sym], piece)
+      result.concat self.send(SPECIAL_MOVES[sym], board, piece)
     end
     
     result.reject(&:nil?)
   end
   
-  def castle_moves(piece)
-    [kingside_castle_move(piece), queenside_castle_move(piece)].reject(&:nil?)
+  def castle_moves(board, piece)
+    [
+      kingside_castle_move(board, piece),
+      queenside_castle_move(board, piece)
+    ].reject(&:nil?)
   end
   
-  def en_passant_moves(piece)
+  def en_passant_moves(board, piece)
     moves = [-1, 1].map do |x|
       from = piece.position
       to = piece.position.increment(y: piece.direction, x: x)
@@ -103,10 +108,10 @@ module MoveGenerator
     moves.reject(&:nil?)
   end
   
-  def kingside_castle_move(piece)
+  def kingside_castle_move(board, piece)
     return unless board.can_castle_kingside? piece.color
 
-    return unless kingside_castle_squares_available? piece
+    return unless kingside_castle_squares_available?(board, piece)
     
     king_from = piece.position.to_s
     king_to = piece.position.increment(x: 2).to_s
@@ -118,10 +123,10 @@ module MoveGenerator
     move
   end
   
-  def queenside_castle_move(piece)
+  def queenside_castle_move(board, piece)
     return unless board.can_castle_queenside? piece.color
 
-    return unless queenside_castle_squares_available? piece
+    return unless queenside_castle_squares_available?(board, piece)
     
     king_from = piece.position.to_s
     king_to = piece.position.increment(x: -2).to_s
@@ -133,14 +138,14 @@ module MoveGenerator
     move
   end
   
-  def kingside_castle_squares_available?(piece)
+  def kingside_castle_squares_available?(board, piece)
     positions = piece.color == :white ? %w[f1 g1] : %w[f8 g8]
     positions.all? do |pos|
       board.piece_at(pos).nil?
     end
   end
   
-  def queenside_castle_squares_available?(piece)
+  def queenside_castle_squares_available?(board, piece)
     positions = piece.color == :white ? %w[b1 c1 d1] : %w[b8 c8 g8]
     positions.all? do |pos|
       board.piece_at(pos).nil?
--- lib/game_modules/move_parser.rb
+++ lib/game_modules/move_parser.rb
@@ -5,10 +5,10 @@ require_relative 'move_generator'
 module MoveParser
   include MoveGenerator
 
-  MOVE_REGEX = /^(0-0)|(0-0-0)|([a-h][1-8]{2}[qrbn]?)$/i
+  MOVE_REGEX = /^(0-0)|(0-0-0)|(([a-h][1-8]){2}[qrbn]?)$/i
 
   def parse_move(move)
-    unless MOVE_REGEX.match(move)
+    unless MOVE_REGEX.match move
       raise ArgumentError, "Invalid move format cannot parse: #{move}"
     end
     
@@ -23,7 +23,7 @@ module MoveParser
     pos = current_player == :white ? 'e1' : 'e8'
     type = move == '0-0' ? :kingside : :queenside
 
-    moves = moves_at pos
+    moves = moves_for_pos(pos, board)
     move = moves.select { |m| m.castle == type }
     
     raise ArgumentError, "Invalid move, cannot castle #{type}" unless move
@@ -31,8 +31,8 @@ module MoveParser
   end
   
   def normal_move(move_str)
-    _, pos, to, promotion = /^(\w{2})(\w{2})(\w?)$/.match move_str
-    moves = gen_move_for pos
+    _, pos, to, promotion = * /^(\w{2})(\w{2})(\w?)$/.match(move_str)
+    moves = moves_for_pos(pos, board)
     move = moves.find { |m| m.to_s.downcase == move_str.downcase }
     raise ArgumentError, "Invalid move: #{move}" unless move
     
