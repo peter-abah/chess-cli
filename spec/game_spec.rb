f69951b2bf05211c84f7ef0872bdb353958a9a1b
Peter Abah <peterabah.ob@gmail.com>
2022-03-16 10:54:57 +0100

    Add tests for #game_over? method of Game class

--- spec/game_spec.rb
+++ spec/game_spec.rb
@@ -393,4 +393,30 @@ describe Game do
       end
     end
   end
+  
+  describe '#game_over?' do
+    context 'when there is a draw' do
+      subject(:game) { described_class.new(fen: '8/k7/8/8/8/6q1/7p/7K w - - 2 5') }
+      
+      it 'returns true' do
+        expect(game.game_over?).to be true
+      end
+    end
+    
+    context 'when it is a new game' do
+      subject(:game) { described_class.new }
+      
+      it 'returns false' do
+        expect(game.game_over?).to be false
+      end
+    end
+    
+    context 'when it is a checkmate' do
+      subject(:game) { described_class.new(fen: 'rrk5/8/8/8/8/8/8/K6R w - - 97 50') }
+      
+      it 'returns true' do
+        expect(game.game_over?).to be true
+      end
+    end
+  end
 end
